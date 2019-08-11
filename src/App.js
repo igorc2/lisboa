9722a4b951a044cd0a808d5a9a1992b860604858
igorc2 <igorc2@gmail.com>
2019-08-11 11:36:41 -0300

    initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import NavBar from './components/layout/Navbar'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <NavBar/>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
