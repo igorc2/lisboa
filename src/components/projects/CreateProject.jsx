835543e0289b850e2003f6980e32c65547ab0acc
igorc2 <igorc2@gmail.com>
2019-08-12 01:01:30 -0300

    firebase connection

--- src/components/projects/CreateProject.jsx
+++ src/components/projects/CreateProject.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { createProject } from '../../store/actions/projectActions'
 
-export default class CreateProject extends Component {
+class CreateProject extends Component {
   state = {
     title: '',
     content: '',
@@ -14,7 +16,8 @@ export default class CreateProject extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
+    //console.log(this.state);
+    this.props.createProject(this.state);
   }
 
   render() {
@@ -39,3 +42,11 @@ export default class CreateProject extends Component {
     )
   }
 }
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    createProject: (project) => dispatch(createProject(project))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(CreateProject)
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -7,6 +7,16 @@ const initState = {
 };
 
 const projectReducer = (state = initState, action) => {
+  switch (action.type) {
+    case 'CREATE_PROJECT':
+      console.log('cre', action.project)
+      return state;
+    case 'CREATE_PROJECT_ERROR':
+      console.error('err: ', action.err)
+      return state;
+    default: 
+      break;
+  }
   return state;
 }
 
